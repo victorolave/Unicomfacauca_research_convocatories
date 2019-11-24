4d811a9b9543f3f856534772a0ada4aaafc247e0
Alejandro Mayor Dk <macmdf@gmail.com>
2019-11-23 20:37:02 -0500

    CreateModels

---
+++ backend/app/Models/IdentificationType.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class IdentificationType extends Model {
+}
+
+module.exports = IdentificationType
---
+++ backend/app/Models/ProjectRol.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class ProjectRol extends Model {
+}
+
+module.exports = ProjectRol
