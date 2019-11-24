913e1cbc3bf3c00640bff50ad3bfc16e2fcdbe2d
Alejandro Mayor Dk <macmdf@gmail.com>
2019-11-23 23:35:13 -0500

    Routes

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -25,3 +25,45 @@ Route.group(() => {
     Route.get('getUser/:id', 'UserController.show')
 
 }).prefix('users')
+
+Route.group(() => {
+    Route.post('create', 'ConvocatoryController.create')
+    Route.post('update','ConvocatoryController.update')
+    Route.post('destroy','ConvocatoryController.destroy')
+}).prefix('Convocatories')
+
+Route.group(() => {
+    Route.post('create', 'EntityController.create')
+    Route.post('update','EntityController.update')
+    Route.post('destroy','EntityController.destroy')
+}).prefix('Entities')
+
+Route.group(() => {
+    Route.post('create', 'PersonController.create')
+    Route.post('update','PersonController.update')
+    Route.post('destroy','PersonController.destroy')
+}).prefix('Persons')
+
+Route.group(() => {
+    Route.post('create', 'ProjectController.create')
+    Route.post('update','ProjectController.update')
+    Route.post('destroy','ProjectController.destroy')
+}).prefix('Projects')
+
+Route.group(() => {
+    Route.post('create', 'ResearcherController.create')
+    Route.post('update','ResearcherController.update')
+    Route.post('destroy','ResearcherController.destroy')
+}).prefix('Researches')
+
+Route.group(() => {
+    Route.post('create', 'ResearchGroupController.create')
+    Route.post('update','ResearchGroupController.update')
+    Route.post('destroy','ResearchGroupController.destroy')
+}).prefix('ResearchesGroup')
+
+Route.group(() => {
+    Route.post('create', 'ResearchSeederController.create')
+    Route.post('update','ResearchSeederController.update')
+    Route.post('destroy','ResearchSeederController.destroy')
+}).prefix('ResearchesSeeder')
