d7a5be92c3b4d946c70b8a1d0759c3f48515b0f9
victorolave <victorolave1131@gmail.com>
2019-11-25 17:48:44 -0500

    Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <Navbar/>
+    <v-content>
+      <router-view />
+    </v-content>
+    
+  </v-app>
+</template>
+
+<script>
+//Components
+import Navbar from './components/Layout/Navbar';
+
+export default {
+  name: "App",
+
+  components: {
+    Navbar,
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ frontend/src/components/Layout/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-toolbar>
+      <v-toolbar-title></v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn dark class="blue" style="margin: 15px">Registrate</v-btn>
+      <v-btn class="success">Ingresar</v-btn>
+      
+
+    </v-toolbar>
+  </div>
+</template>
